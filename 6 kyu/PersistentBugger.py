9c0bfe1256771cf11c9ca50db4c2e1394c5bf748
jahnito <jahnito@gmail.com>
2024-02-25 20:17:29 +0500

    add 5 tasks

---
+++ 6 kyu/PersistentBugger.py
@@ -0,0 +1,31 @@
+'''
+https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/python
+
+Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+For example (Input --> Output):
+
+39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+4 --> 0 (because 4 is already a one-digit number)
+'''
+
+def persistence(n):
+    def sumar(k):
+        res = 1
+        while k > 0:
+            res *= k % 10
+            k = k // 10
+        return res
+    
+    c = 0
+    while n > 9:
+        c += 1
+        n = sumar(n)
+    
+    return c
+
+
+
+if __name__ == '__main__':
+    print(persistence(10))
